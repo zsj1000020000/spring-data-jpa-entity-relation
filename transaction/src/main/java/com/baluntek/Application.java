57ac862f3a12fb42a1b86c631bec76bef4842bc8
shaojun.zheng <shaojun.zheng@baluntek.com>
2015-01-30 14:15:19 +0800

    edit application.java

--- transaction/src/main/java/com/baluntek/Application.java
+++ transaction/src/main/java/com/baluntek/Application.java
@@ -1,21 +1,13 @@
 package com.baluntek;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.baluntek.service.UserService;
 
 @SpringBootApplication
-public class Application  {
+public class Application {
 
-	@Autowired
-	private UserService userService;
-	
-	
-	
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-			
-   
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
 }
